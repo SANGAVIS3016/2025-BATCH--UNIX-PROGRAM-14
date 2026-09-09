3aace0d3a8150e521718965aecfcfc0bab977937
SANGAVIS3016 <sangavis@rvsgroup.com>
2026-09-09 10:26:38 +0530

    Update starter.sh

--- starter.sh
+++ starter.sh
@@ -1,28 +1,18 @@
 #!/bin/bash
 
 # Linux User Management Assignment
-
 # Create a user, set the password, and delete the user.
 
 USERNAME="student_test"
 PASSWORD="Student@123"
 
-# TODO 1:
+# TODO 1: Create the user
 
-# Create the user.
 
-# Use the appropriate Linux command.
+# TODO 2: Set the password
 
-# TODO 2:
 
-# Set the password of the user.
+# TODO 3: Delete the user
 
-# Hint: passwd can read the password from standard input.
-
-# TODO 3:
-
-# Delete the user.
-
-# Use the appropriate Linux command.
 
 echo "User management operations completed."
