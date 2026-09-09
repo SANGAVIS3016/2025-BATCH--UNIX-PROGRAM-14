1e4abbabafdd81de46cf45d9f8b1b9acc77b15ea
SANGAVIS3016 <sangavis@rvsgroup.com>
2026-09-09 14:14:28 +0530

    Update starter.sh

--- starter.sh
+++ starter.sh
@@ -1,15 +1,1 @@
-#!/bin/bash
 
-# Define the test user
-USERNAME="student_test"
-
-# 1. Add user using useradd
-useradd "$USERNAME"
-
-# 2. Set user password using chpasswd (or passwd)
-echo "$USERNAME:Password123!" | chpasswd
-
-# 3. Delete user using userdel
-userdel -r "$USERNAME"
-
-echo "User management operations completed."
